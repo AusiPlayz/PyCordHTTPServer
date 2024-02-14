c13957e2918998ec8ac93e04376eb11fe2c2de90
AustrianNoah <151822733+AustrianNoah@users.noreply.github.com>
2024-02-14 11:31:55 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup
+
+import server
+
+with open(os.path.abspath("./README.md", "r")) as file:
+    readme = file.read()
+
+
+setup(
+    name="PyCordHTTPServer",
+    version=server.__version__,
+    description="Fuck you Biatch",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AusiPlayz",
+    author=server.__author__,
+    license=server.__license__,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    packages=["server"],
+    include_package_data=True,
+    install_requires=["aiohttp", "py-cord"]
+)
